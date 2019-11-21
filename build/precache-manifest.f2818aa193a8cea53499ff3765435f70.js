9ad48fb395e4522d5cf894152de5fc63c8757d6a
Joki7777 <mjoki7777@gmail.com>
2019-11-21 14:32:38 +0500

    asd

---
+++ build/precache-manifest.f2818aa193a8cea53499ff3765435f70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7639f04ed7f3930a539bd920e16a8ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81d2bce5c4f916fff7d4",
+    "url": "/static/css/main.7822d97c.chunk.css"
+  },
+  {
+    "revision": "44769b952df12152abd7",
+    "url": "/static/js/2.b19ceb3b.chunk.js"
+  },
+  {
+    "revision": "81d2bce5c4f916fff7d4",
+    "url": "/static/js/main.897bdbeb.chunk.js"
+  },
+  {
+    "revision": "51d5e2a6f1d0d9a901ed",
+    "url": "/static/js/runtime-main.43a96059.js"
+  }
+]);
